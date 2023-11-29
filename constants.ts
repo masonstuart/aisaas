01c5a0f7dbcc1fc55fa7d60c2e2fedbc6428df67
Mason Stuart <masonstuart321@gmail.com>
2023-11-29 13:14:14 -0500

    first commit

---
+++ constants.ts
@@ -0,0 +1,28 @@
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+
+export const MAX_FREE_COUNTS = 50;
+
+export const tools = [
+  {
+    label: 'Music Generation',
+    icon: Music,
+    href: '/music',
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: '/image',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: '/video',
+  },
+ 
+];
